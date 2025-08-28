effbe8cb97f7364d6e244e9be31c4f891b2f3b7a
oono0908 <damian_wkids@yahoo.co.jp>
2025-08-28 14:53:03 +0900

    completed contact and improve other function

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,54 @@
+// drawerの開閉
 $(function () {
   $(".js-hamberger,.js-drawer,.js-drawer__link").click(function() {
-     e.preventDefault();
      $(".js-hamberger").toggleClass("is-active")
      $(".js-drawer").fadeToggle()
+     return false;
     })
 });
+// drawer__linkクリック時に選択された場所移動する
+$(function() {
+  $('.js-drawer__item a').on('click', function(e) {
+    e.preventDefault();
+    var targetId = $(this).attr('href');
+    var target = $(targetId);
+    if(target.length) {
+      var headerHeight = $('.js-header').outerHeight();
+      var scrollPosition = target.offset().top - headerHeight;
+      $('html, body').animate({
+        scrollTop: scrollPosition
+      }, 600);
+    }
+  });
+});
 
+// swiper
 const mySwiper = new Swiper ('.swiper', {
   loop: true,
   autoplay: { 
   delay: 3000,
   },
+});
+
+// topBtnクリック時にtopに戻る
+$(function() {
+  $('.js-top-btn').on('click', function(e) {
+    $('html, body').animate({ scrollTop: 0 }, 600); 
+    return false;
+  });
+});
+
+// headerの背景変化
+$(function() {
+  $(window).on('scroll', function() {
+    var conceptTop = $('.js-concept').offset().top;
+    var scroll = $(window).scrollTop();
+    if (scroll >= conceptTop) {
+      $('.js-header').css('background-color', "white");
+      $('.js-top-btn').css('display','block');
+    } else {
+      $('.js-header').css('background-color', 'transparent');
+       $('.js-top-btn').css('display','none');
+    }
+  });
 });
